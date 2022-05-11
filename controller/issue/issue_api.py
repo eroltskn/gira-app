7c5c3f4d339c4ab1224edc4703befdbc153308bd
eroltskn <eroltaskinnn@gmail.com>
2022-05-11 16:35:20 +0300

    issue change log endpoint moved to issueapi

--- controller/issue/issue_api.py
+++ controller/issue/issue_api.py
@@ -10,14 +10,16 @@ from helper.format_helper import parse_cerberus_error_messages
 
 from sqlalchemy import and_
 from sqlalchemy.sql.expression import false
-from models.models import Issue, Project, db
+from models.models import Issue, IssueChangeLog, db
 
 from schema.error_schema import ErrorResponse
 from schema.info_schema import InfoResponse
 from schema.issue.issue_create_schema import ISSUE_POST_REQUEST_SCHEMA
 from schema.issue.issue_create_schema import IssuePostRequest
 from schema.issue.issue_get_schema import IssueGetResponse
-from schema.issue.issue_update_schema import IssuePatchRequest, ISSUE_PATCH_REQUEST_SCHEMA
+from schema.issue.issue_change_log_schema import IssueChangeLogPostRequest, \
+    ISSUE_CHANGE_LOG_POST_REQUEST_SCHEMA
+from schema.issue.issue_log_get_schema import IssueLogGetResponse
 
 issue_api_endpoint = Blueprint('issue', __name__)
 
@@ -117,58 +119,6 @@ def issue_post_method():
         return jsonify(error), 500
 
 
-@issue_api_endpoint.route("issue/<issue_id>", methods=["PATCH"])
-@validate_request_input
-@jwt_required()
-@requires_owner_role
-@requires_gira_role(roles=[1, 2])
-def issue_patch_method(issue_id):
-    try:
-        """ Request body payload """
-        payload = request.json
-        payload_model = IssuePatchRequest(payload=payload)
-
-        """ Validate payload """
-        validator = Validator(ISSUE_PATCH_REQUEST_SCHEMA, allow_unknown=False)
-        validation_result = validator.validate(payload)
-
-        if not validation_result:
-            error_message = parse_cerberus_error_messages(validator)
-            error_model = ErrorResponse(errors=error_message)
-            error = error_model.__dict__
-
-            return jsonify(error), 400
-
-        issue = Issue.query.get(issue_id)
-
-        if payload_model.name:
-            issue.name = payload_model.name
-
-        if payload_model.description:
-            issue.description = payload_model.description
-
-        if payload_model.issue_type_id:
-            issue.issue_type_id = payload_model.issue_type_id
-
-        if payload_model.issue_status_id:
-            issue.issue_status_id = payload_model.issue_status_id
-
-        db.session.commit()
-
-        """ Result response model """
-        response_model = InfoResponse()
-
-        auth_response = response_model.__dict__
-
-        return jsonify(auth_response), 200
-    except Exception as e:
-        error_model = ErrorResponse(errors='unknown error')
-        error = error_model.__dict__
-        logger.error(str(e))
-
-        return jsonify(error), 500
-
-
 @issue_api_endpoint.route("issue/<issue_id>", methods=["DELETE"])
 @validate_request_input
 @jwt_required()
@@ -199,6 +149,109 @@ def issue_delete_method(issue_id):
         auth_response = response_model.__dict__
 
         return jsonify(auth_response), 204
+    except Exception as e:
+        error_model = ErrorResponse(errors='unknown error')
+        error = error_model.__dict__
+        logger.error(str(e))
+
+        return jsonify(error), 500
+
+
+@issue_api_endpoint.route("issue/change_log/<issue_id>", methods=["GET"])
+@validate_request_input
+@jwt_required()
+@requires_gira_role(roles=[1, 2])
+def issue_change_log_get_method(issue_id):
+    try:
+
+        issue_log_list = db.session.query(Issue, IssueChangeLog).join(IssueChangeLog).filter(
+            and_(Issue.id == int(issue_id), Issue.is_deleted == false())).all()
+
+        if not issue_log_list:
+            error_model = ErrorResponse(errors=[
+                'No issue log found'
+            ])
+            error = error_model.__dict__
+
+            return jsonify(error), 404
+
+        response_list = []
+
+        for issue_item in issue_log_list:
+            temp_object = {
+                'id': issue_item.IssueChangeLog.id,
+                'field_name': issue_item.IssueChangeLog.field_name,
+                'new_value': issue_item.IssueChangeLog.new_value,
+                'old_value': issue_item.IssueChangeLog.old_value,
+                'user_id': issue_item.IssueChangeLog.user_id,
+                'issue_id': issue_item.IssueChangeLog.issue_id,
+                'created': issue_item.IssueChangeLog.created.strftime("%m/%d/%Y, %H:%M:%S"),
+                'modified': issue_item.IssueChangeLog.modified.strftime("%m/%d/%Y, %H:%M:%S"),
+            }
+            response_list.append(temp_object)
+
+        """ Result response model """
+        response_model = IssueLogGetResponse(response_list)
+
+        auth_response = response_model.__dict__
+
+        return jsonify(auth_response), 200
+    except Exception as e:
+        error_model = ErrorResponse(errors='unknown error')
+        error = error_model.__dict__
+        logger.error(str(e))
+
+        return jsonify(error), 500
+
+
+@issue_api_endpoint.route("issue/change_log", methods=["POST"])
+@validate_request_input
+@jwt_required()
+@requires_gira_role(roles=[1, 2])
+def issue_change_log_post_method():
+    try:
+        """ Request body payload """
+        payload = request.json
+        payload_model = IssueChangeLogPostRequest(payload=payload)
+
+        """ Validate payload """
+        validator = Validator(ISSUE_CHANGE_LOG_POST_REQUEST_SCHEMA, allow_unknown=False)
+        validation_result = validator.validate(payload)
+
+        if not validation_result:
+            error_message = parse_cerberus_error_messages(validator)
+            error_model = ErrorResponse(errors=error_message)
+            error = error_model.__dict__
+
+            return jsonify(error), 400
+
+        issue = db.session.query(Issue).filter(
+            and_(Issue.id == int(payload_model.issue_id), Issue.is_deleted == false())).first()
+
+        if not issue:
+            error_model = ErrorResponse(errors=[
+                'No issue found'
+            ])
+            error = error_model.__dict__
+
+            return jsonify(error), 404
+
+        issue_log = IssueChangeLog(issue_id=payload_model.issue_id,
+                                   user_id=payload_model.user_id,
+                                   new_value=payload_model.new_value,
+                                   old_value=issue[payload_model.field_name],
+                                   field_name=payload_model.field_name)
+
+        issue[payload_model.field_name] = payload_model.new_value
+        db.session.add(issue_log)
+        db.session.commit()
+
+        """ Result response model """
+        response_model = InfoResponse()
+
+        auth_response = response_model.__dict__
+
+        return jsonify(auth_response), 201
     except Exception as e:
         error_model = ErrorResponse(errors='unknown error')
         error = error_model.__dict__
--- helper/endpoint_binder.py
+++ helper/endpoint_binder.py
@@ -5,7 +5,6 @@ from controller.user.register import user_register_endpoint
 
 from controller.project.project_api import project_api_endpoint
 from controller.issue.issue_api import issue_api_endpoint
-from controller.issue.issue_change_log import issue_change_log_endpoint
 
 
 def bind_endpoints():
@@ -15,4 +14,3 @@ def bind_endpoints():
 
     current_app.register_blueprint(project_api_endpoint, url_prefix='/v1/')
     current_app.register_blueprint(issue_api_endpoint, url_prefix='/v1/')
-    current_app.register_blueprint(issue_change_log_endpoint, url_prefix='/v1/')
